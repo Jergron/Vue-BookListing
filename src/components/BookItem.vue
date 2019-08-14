2c8dcb75ace4df781d31d9ed07e8fb41f2bd7b1f
Jeremy Grondahl <jgrondahl@Jeremy-Grondahl.local>
2019-08-14 16:33:51 -0500

    finished Creating a Child Component module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li>{{book.title}}: {{author.title}}</li>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'BookItem',
+    props: ['book']
+}
+</script>
+<style scoped>
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+</style>
